0e690081140ae5ea4db7835a4cbe8a004d1ee0ee
JHu-sc <jhu@supercars.com>
2025-04-04 08:05:41 +1000

    test: changing local repo file location

--- src/main.py
+++ src/main.py
@@ -1,1 +1,7 @@
-print("Hello World")
+#Testing moving folder to different local location doesn't affect sync to Git Hub
+def main(): 
+    print("Personal Project: To Be Determined")
+    
+if __name__ == "__main__":
+    main()
+    
